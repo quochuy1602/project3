69f7eab92c3e893cb824df2e099fb23f6cad9000
Quoc Huy <quochuy1602@gmail.com>
2022-08-13 08:54:06 +0700

    commit code

---
+++ udagram-api-feed/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'postgres',
+  'password': 'abcd1234',
+  'database': 'postgres',
+  'host': 'postgres.cwjsurmsxfvl.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'chuongnv',
+  'aws_media_bucket': 'chuongnv',
+  'url': 'http://localhost:8100',
+  'jwt': {
+    'secret': 'u5+j7ayrn4nXBvlkfdx68ssOoUXHJWy5O9ibYJJD'
+  },
+};
