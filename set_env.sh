29bf94b2d9cecf317b2bfee6aefeab0b10a98663
Quoc Huy <quochuy1602@gmail.com>
2022-08-25 14:58:04 +0700

    up

--- set_env.sh
+++ set_env.sh
@@ -1,20 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-# export POSTGRES_USERNAME=postgres
-# export POSTGRES_PASSWORD=abcd1234
-# export POSTGRES_HOST=postgres.cwjsurmsxfvl.us-east-1.rds.amazonaws.com
-# export POSTGRES_DB=postgres
-# export AWS_BUCKET=arn:aws:s3:::test-nd9990-dev-wc
-# export AWS_REGION=us-east-1
-# export AWS_PROFILE=default
-# export JWT_SECRET=testing
-# export URL=http://localhost:8100
-setx POSTGRES_USERNAME postgres
-setx POSTGRES_PASSWORD abcd1234
-setx POSTGRES_HOST=postgres.cwjsurmsxfvl.us-east-1.rds.amazonaws.com
-setx POSTGRES_DB postgres
-setx AWS_BUCKET chuongnv
-setx AWS_REGION us-east-1
-setx AWS_PROFILE chuongnv
-setx JWT_SECRET j7ayrn4nXBvlkfdx68ssOoUXHJWy5O9ibYJJD
-setx URL http://localhost:8100
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=abcd344
+export POSTGRES_HOST=postgres.3424.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::test-nd9990-dev-wc
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
